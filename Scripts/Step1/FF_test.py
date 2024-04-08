1f88da1c093621a3a58c24a7841bfe6936408b1d
Hesam Makki <107540794+HMakkiMD@users.noreply.github.com>
2024-04-08 13:50:51 +0100

    Update FF_test.py

--- Scripts/Step1/FF_test.py
+++ Scripts/Step1/FF_test.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 '''
+This is the fifth code to run in Step 1.
 
 This code checks if the total energy from the force field at each scan point matches the DFT-calculated torsional potential.
 It plots both values (V_tot and V_DFT) at each scan point for all torsions in the oligomer structure separately which can be seen
